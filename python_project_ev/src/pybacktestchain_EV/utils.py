15561248a7668502e240841a8e6a89d3f18c0887
EnzoVCode <enzo.volpato09@gmail.com>
2025-01-10 18:37:27 +0100

    :feat: modified the random generated blockchain names

--- python_project_ev/src/pybacktestchain_EV/utils.py
+++ python_project_ev/src/pybacktestchain_EV/utils.py
@@ -1,24 +1,27 @@
 import random
 
-# List of animals, professions, and colors
-animals = [
-    "Eagle", "Tiger", "Lion", "Wolf", "Bear", "Falcon", "Shark", "Panther", "Leopard", "Cheetah", 
-    "Hawk", "Fox", "Owl", "Cobra", "Jaguar", "Horse", "Elephant", "Dolphin", "Gorilla", "Lynx"
+# List of famous guitar players, bike names, and gear brands
+guitar_players = [
+    "Hendrix", "Clapton", "Page", "SRV", "Gilmour", 
+    "VanHalen", "Satriani", "Slash", "Santana", "Blackmore", 
+    "Hammett", "Iommi", "Smith", "Beck", "Young"
 ]
 
-professions = [
-    "Carpenter", "Engineer", "Doctor", "Pilot", "Farmer", "Artist", "Blacksmith", "Chef", "Teacher", "Mechanic",
-    "Architect", "Scientist", "Soldier", "Nurse", "Firefighter", "Plumber", "Astronaut", "Tailor", "Photographer", "Lawyer"
+bike_names = [
+    "FatBoy", "RoadKing", "Scout", "Monster", 
+    "Bonneville", "R1", "Ninja", "nineT", 
+    "Hayabusa", "GoldWing", "FatBob", "ElectraGlide",
 ]
 
-colors = [
-    "Red", "Blue", "Green", "Yellow", "Black", "White", "Purple", "Orange", "Brown", "Grey", 
-    "Pink", "Violet", "Crimson", "Turquoise", "Gold", "Silver", "Amber", "Magenta", "Teal", "Indigo"
+gear_brands = [
+    "Fender", "Gibson", "Marshall", "Mesa Boogie", "Ibanez", "PRS", 
+    "Orange", "Gretsch", "Dunlop", "Boss"
 ]
 
 # Function to generate a random name
 def generate_random_name():
-    animal = random.choice(animals)
-    profession = random.choice(professions)
-    color = random.choice(colors)
-    return f"{color}{animal}{profession}"
+    player = random.choice(guitar_players)
+    bike = random.choice(bike_names)
+    gear = random.choice(gear_brands)
+    return f"{player}-{bike}-{gear}"
+
